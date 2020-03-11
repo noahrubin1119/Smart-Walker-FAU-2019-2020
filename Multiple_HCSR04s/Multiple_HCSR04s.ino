46dd1a5df2102bac14eaba5722a971181b62735f
noahrubin1119 <35118067+noahrubin1119@users.noreply.github.com>
2020-03-11 17:15:29 -0400

    Update Multiple_HCSR04s.ino

--- Multiple_HCSR04s/Multiple_HCSR04s.ino
+++ Multiple_HCSR04s/Multiple_HCSR04s.ino
@@ -22,8 +22,11 @@
 float distance0, distance1, distance2, distance3;
 
 void setup() {
-  
-  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
@@ -45,5 +48,38 @@ void loop() {
   Serial.print("D 3: " + String(int(distance3)) + "cm  ");
   Serial.println();
 
-  delay(500);
+  if(distance0 < 10){
+    digitalWrite(2, HIGH);
+    Serial.print("X ");
+  }
+  else{
+    digitalWrite(2, LOW);
+    Serial.print("O ");
+  }
+   if(distance1 < 10){
+    digitalWrite(3, HIGH);
+    Serial.print("X ");
+  }
+  else{
+    digitalWrite(3, LOW);
+    Serial.print("O ");
+  }
+   if(distance2 < 10){
+    digitalWrite(4, HIGH);
+    Serial.print("X ");
+  }
+  else{
+    digitalWrite(4, LOW);
+    Serial.print("O ");
+  }
+   if(distance3 < 10){
+    digitalWrite(5, HIGH);
+    Serial.print("X ");
+  }
+  else{
+    digitalWrite(5, LOW);
+    Serial.print("O ");
+  }
+
+  delay(150);
 }
